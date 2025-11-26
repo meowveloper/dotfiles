d189847c5659879df3fe57ad4645fcaba158bbaf
meowveloper <meowveloper.dev@gmail.com>
2025-11-26 23:00:29 +0700

    fix it again

--- start-up/install-extra-packages.sh
+++ start-up/install-extra-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SOURCEDIR="${HOME}/.local/share/chezmoi"
+PKGLIST="${SOURCEDIR}/start-up/extra-packages.txt"
 echo "installing extra packages."
 
 cat "$PKGLIST" | yay -S - --noconfirm --needed
